abad4364f098dce1a2350622a0a092e5d3527965
Lingjun Zhang <1156404931@qq.com>
2026-05-18 18:04:49 +0800

    Update nebula.sh

--- nebula.sh
+++ nebula.sh
@@ -2,7 +2,7 @@
 # LLaMA-Factory 8卡分布式训练脚本
 
 # method1
-llamafactory-cli train --config ./configs/ad_bev_ex3_2.yaml
+llamafactory-cli train --config ./configs/ad_bev_v4.yaml
 
 # method2
-torchrun --nproc_per_node=8 src/train.py --config ./configs/ad_bev_ex3_2.yaml
+torchrun --nproc_per_node=8 src/train.py --config ./configs/ad_bev_v4.yaml
